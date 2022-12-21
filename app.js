29c0430ca9ef277483f3555a7c340422ca6b1fff
jacopo <jacopotrapani89@gmail.com>
2022-12-21 15:29:28 +0100

    changed request limit

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ app.use(cors({
 }));
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit:"50mb"}));
+app.use(express.urlencoded({ extended: true, limit:"50mb", parameterLimit:50000000000 }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
